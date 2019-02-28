03f0b3e2233585bc7a83ceb3b215cd46b0b7b408
duanyuwen <duanyuwen@meituan.com>
2019-02-28 16:14:04 +0800

    support talos build

--- template/config/index.js
+++ template/config/index.js
@@ -46,12 +46,12 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../build/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../build'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: process.env.PUBLIC_URL || '/',
 
     /**
      * Source Maps
